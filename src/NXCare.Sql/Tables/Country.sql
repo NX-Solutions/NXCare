d950cd30bf1a3c9b66fef20fc681d98a4ceb55bd
Maxime Poulain <maxime.poulain@outlook.be>
2020-10-13 21:59:33 +0200

    Introduce sql project and country table with a seed script #5

---
+++ src/NXCare.Sql/Tables/Country.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Country] (
+  Id int IDENTITY(1,1) NOT NULL,
+  Alpha2Code           varchar(2)               NOT NULL,
+  NameTranslationKey   varchar(80)              NOT NULL,
+  Alpha3Code           varchar(3)NOT            NULL,
+  NumericCode          int NOT                  NULL,
+  PhoneCode            int NOT                  NULL,
+  CONSTRAINT           [PK_Country]             PRIMARY KEY CLUSTERED ([Id] ASC),
+  CONSTRAINT           [UC_Country_Alpha2Code]  UNIQUE NONCLUSTERED ([Alpha2Code] ASC)
+) 

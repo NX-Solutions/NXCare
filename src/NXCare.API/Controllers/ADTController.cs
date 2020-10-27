bead726649c2b50295de4cf992df61eab78a9d6a
Maxime Poulain <maxime.poulain@outlook.be>
2020-10-27 18:31:00 +0100

    Add an ADT controller

---
+++ src/NXCare.API/Controllers/ADTController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NXCare.Domain.DTO;
+using NXCare.Domain.Enums;
+using NXCare.Domain.Interfaces.Services;
+
+namespace NXCare.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ADTController : ControllerBase
+    {
+        private readonly IPatientCreationService patientCreationService;
+
+        public ADTController(IPatientCreationService patientCreationService)
+        {
+            this.patientCreationService = patientCreationService;
+        }
+
+        [HttpPost("[action]")]
+        public async Task<ActionResult> Admission(Patient patient)
+        {
+            try
+            {
+                var (patientCreationResults, publicId) = await patientCreationService.AddOrUpdatePatientAsync(patient, "TEST SOURCE").ConfigureAwait(false);
+
+                if (patientCreationResults == PatientCreationResults.Error)
+                {
+                    throw new Exception();
+                }
+
+                patient.Id = publicId;
+
+                return patientCreationResults == PatientCreationResults.Created ? Created(string.Empty, patient) : StatusCode(StatusCodes.Status200OK, patient);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError , "bidon");
+            }
+        }
+    }
+}

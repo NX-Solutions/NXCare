192f88629395f43cdb7b02748ef56d29ee6b7d2e
Maxime Poulain <maxime.poulain@outlook.be>
2020-11-12 23:45:41 +0100

    Sql: make PublicId's columns indexed

--- src/NXCare.Sql/Tables/Patient.sql
+++ src/NXCare.Sql/Tables/Patient.sql
@@ -1,21 +1,21 @@
 ﻿CREATE TABLE [dbo].[Patient]
 (
     [Id]            INT                   NOT NULL PRIMARY KEY IDENTITY(1,1),
-    [PublicId]      UNIQUEIDENTIFIER      NOT NULL DEFAULT NEWID(), 
-    [NationalId]    NVARCHAR(50)          NULL, 
-    [LastName]      NVARCHAR(50)          NOT NULL, 
-    [MiddleName]    NVARCHAR(50)          NULL, 
-    [FirstName]     NVARCHAR(50)          NOT NULL, 
-    [BirthDate]     DATETIME2             NULL, 
-    [NationalityId] INT                   NULL, 
-    [LanguageId]    INT                   NULL, 
-    [Sex]           INT                   NULL, 
-    [ExternalId]    NVARCHAR(50)          NULL, 
-    [CreatedOn]     DATETIME2             NOT NULL DEFAULT GETUTCDATE(), 
-    [ModifiedOn]    DATETIME2             NULL, 
+    [PublicId]      UNIQUEIDENTIFIER      NOT NULL DEFAULT NEWID(),
+    [NationalId]    NVARCHAR(50)          NULL,
+    [LastName]      NVARCHAR(50)          NOT NULL,
+    [MiddleName]    NVARCHAR(50)          NULL,
+    [FirstName]     NVARCHAR(50)          NOT NULL,
+    [BirthDate]     DATETIME2             NULL,
+    [NationalityId] INT                   NULL,
+    [LanguageId]    INT                   NULL,
+    [Sex]           INT                   NULL,
+    [ExternalId]    NVARCHAR(50)          NULL,
+    [CreatedOn]     DATETIME2             NOT NULL DEFAULT GETUTCDATE(),
+    [ModifiedOn]    DATETIME2             NULL,
     [DeletedOn]     DATETIME2             NULL,
-    CONSTRAINT      [FK_Patient_Country]  FOREIGN KEY ([NationalityId]) REFERENCES [Country]([Id]), 
-    CONSTRAINT      [FK_Patient_Language] FOREIGN KEY ([LanguageId]) REFERENCES [Language]([Id]), 
+    CONSTRAINT      [FK_Patient_Country]  FOREIGN KEY ([NationalityId]) REFERENCES [Country]([Id]),
+    CONSTRAINT      [FK_Patient_Language] FOREIGN KEY ([LanguageId]) REFERENCES [Language]([Id]),
 )
 GO
 CREATE INDEX [IX_Patient_Country] ON [dbo].[Patient] ([NationalityId])
@@ -23,3 +23,5 @@ GO
 CREATE INDEX [IX_Patient_Language] ON [dbo].[Patient] ([LanguageId])
 GO
 CREATE UNIQUE INDEX [IX_Patient_ExternalId] ON [dbo].[Patient] ([ExternalId])
+GO
+CREATE INDEX [IX_Patient_PublicId] ON [dbo].[Patient] ([PublicId])
--- src/NXCare.Sql/Tables/Physician.sql
+++ src/NXCare.Sql/Tables/Physician.sql
@@ -1,26 +1,28 @@
 ﻿CREATE TABLE [dbo].[Physician]
 (
 	[Id]               INT                     NOT NULL PRIMARY KEY IDENTITY(1,1),
-    [PublicId]         UNIQUEIDENTIFIER        NOT NULL DEFAULT NEWID(), 
-    [NationalId]       NVARCHAR(50)            NULL, 
-    [LastName]         NVARCHAR(50)            NOT NULL, 
-    [MiddleName]       NVARCHAR(50)            NULL, 
-    [FirstName]        NVARCHAR(50)            NOT NULL, 
-    [BirthDate]        DATETIME2               NULL, 
-    [NationalityId]    INT                     NULL, 
-    [LanguageId]       INT                     NULL, 
-    [Sex]              INT                     NULL, 
-    [MedicalLicenseId] NVARCHAR(50)            NOT NULL, 
+    [PublicId]         UNIQUEIDENTIFIER        NOT NULL DEFAULT NEWID(),
+    [NationalId]       NVARCHAR(50)            NULL,
+    [LastName]         NVARCHAR(50)            NOT NULL,
+    [MiddleName]       NVARCHAR(50)            NULL,
+    [FirstName]        NVARCHAR(50)            NOT NULL,
+    [BirthDate]        DATETIME2               NULL,
+    [NationalityId]    INT                     NULL,
+    [LanguageId]       INT                     NULL,
+    [Sex]              INT                     NULL,
+    [MedicalLicenseId] NVARCHAR(50)            NOT NULL,
     [SpecialityId]     INT                     NULL,
-    [CreatedOn]        DATETIME2               NOT NULL DEFAULT GETUTCDATE(), 
-    [ModifiedOn]       DATETIME2               NULL, 
+    [CreatedOn]        DATETIME2               NOT NULL DEFAULT GETUTCDATE(),
+    [ModifiedOn]       DATETIME2               NULL,
     [DeletedOn]        DATETIME2               NULL,
-    [ExternalId] NVARCHAR(50) NULL, 
-    CONSTRAINT         [FK_Physician_Country]  FOREIGN KEY ([NationalityId]) REFERENCES [Country]([Id]), 
-    CONSTRAINT         [FK_Physician_Language] FOREIGN KEY ([LanguageId]) REFERENCES [Language]([Id]), 
+    [ExternalId] NVARCHAR(50) NULL,
+    CONSTRAINT         [FK_Physician_Country]  FOREIGN KEY ([NationalityId]) REFERENCES [Country]([Id]),
+    CONSTRAINT         [FK_Physician_Language] FOREIGN KEY ([LanguageId]) REFERENCES [Language]([Id]),
 )
 
 GO
-CREATE INDEX [IX_Physician_Country] ON [dbo].[Patient] ([NationalityId])
+CREATE UNIQUE INDEX [IX_Physician_PublicId] on [dbo].[Physician] ([PublicId])
 GO
-CREATE INDEX [IX_Physician_Language] ON [dbo].[Patient] ([LanguageId])
+CREATE INDEX [IX_Physician_Country] ON [dbo].[Physician] ([NationalityId])
+GO
+CREATE INDEX [IX_Physician_Language] ON [dbo].[Physician] ([LanguageId])
